7d29cae6ae573838734362338d98870fa0ca9dca
jhonManuelGil <jhonmanuelg6@gmail.com>
2025-11-03 01:24:15 -0300

    Uploading: Python course update in parameters

--- Python/fundamentos_de_funciones.py
+++ Python/fundamentos_de_funciones.py
@@ -18,9 +18,31 @@
 
 
 
-def suma(a,b):
-    resultado = a + b
-    print(f"las suma de {a} + {b} es: {resultado}")
+# def suma(a,b):
+#     resultado = a + b
+#     print(f"las suma de {a} + {b} es: {resultado}")
     
 
-suma(12,89)
+# suma(12,89)
+
+
+# ============== argumentos posicionales =============
+ 
+# def nomb_codena(nombre, apellido):
+#     cadena = f"{nombre} {apellido}" # concatena las dos cadenas
+#     print(cadena)
+
+
+# nomb_codena("Isaac", "Gil")
+# nomb_codena("Gil", "Isaac")
+
+
+def saludar_pers(nombre, saludo):
+    mensaje = f"{saludo}, Bienvenido {nombre} a la clase de funciones" # crea la cedena con parametros
+    print(mensaje)
+
+saludar_pers(saludo="Hola", nombre="Isaac") # llamando la funcion
+
+
+# ============== valores por defectos en parametros =============
+
