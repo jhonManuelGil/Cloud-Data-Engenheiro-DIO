e7740b0f35046ddf9129b2548776c846b501df1c
jhonManuelGil <jhonmanuelg6@gmail.com>
2025-11-03 00:26:47 -0300

    Uploading: Fundamentals of Functions in Python

---
+++ Python/fundamentos_de_funciones.py
@@ -0,0 +1,26 @@
+
+# Funciones en Python
+
+
+# def saludo():
+#     print("Hola mundo")
+
+# saludo() # llamando la funcion
+
+ # ============== argumentos y párametros =============
+ 
+# def saludar_pers(nombre):
+#     mensaje = f"Hola, Bienvenido {nombre} a la clase de funciones en Python"
+#     print(mensaje)
+
+
+# saludar_pers("Jhon Gil") # llamando la funcion con argumento
+
+
+
+def suma(a,b):
+    resultado = a + b
+    print(f"las suma de {a} + {b} es: {resultado}")
+    
+
+suma(12,89)
